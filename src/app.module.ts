abc1fb3ffac4b56742898751055785c2bd846ea9
mr2079 <105517282+mr2079@users.noreply.github.com>
2024-03-05 00:50:58 +0330

    Add database configurar

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { \src\infrastructure\config\environmentConfigModule } from './src/infrastructure/config/environment-config/src/infrastructure/config/environment-config.module';
+import { \src\infrastructure\config\environmentConfigService } from './src/infrastructure/config/environment-config/src/infrastructure/config/environment-config.service';
+import { EnvironmentConfigService } from './infrastructure/config/environment-config/environment-config.service';
+import { EnvironmentConfigModule } from './infrastructure/config/environment-config/environment-config.module';
+import { TypormModule } from './infrastructure/config/typorm/typorm.module';
+import { TypeormModule } from './infrastructure/config/typeorm/typeorm.module';
 
 @Module({
-  imports: [],
+  imports: [\src\infrastructure\config\environmentConfigModule, EnvironmentConfigModule, TypormModule, TypeormModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, \src\infrastructure\config\environmentConfigService, EnvironmentConfigService],
 })
 export class AppModule {}
