eab1c519d2938eab848561290e289004d4f0728d
mr2079 <mohammadalikhoshdooni@yahoo.com>
2024-03-05 11:47:24 +0330

    Fix auto import problems in app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { \src\infrastructure\config\environmentConfigModule } from './src/infrastructure/config/environment-config/src/infrastructure/config/environment-config.module';
-import { \src\infrastructure\config\environmentConfigService } from './src/infrastructure/config/environment-config/src/infrastructure/config/environment-config.service';
 import { EnvironmentConfigService } from './infrastructure/config/environment-config/environment-config.service';
 import { EnvironmentConfigModule } from './infrastructure/config/environment-config/environment-config.module';
-import { TypormModule } from './infrastructure/config/typorm/typorm.module';
 import { TypeormModule } from './infrastructure/config/typeorm/typeorm.module';
 
 @Module({
-  imports: [\src\infrastructure\config\environmentConfigModule, EnvironmentConfigModule, TypormModule, TypeormModule],
+  imports: [EnvironmentConfigModule, EnvironmentConfigModule, TypeormModule],
   controllers: [AppController],
-  providers: [AppService, \src\infrastructure\config\environmentConfigService, EnvironmentConfigService],
+  providers: [AppService, EnvironmentConfigService, EnvironmentConfigService],
 })
 export class AppModule {}
